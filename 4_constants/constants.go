c62fc662666c4f4f0b543bc7439b2e463e1d7472
Sujal Patel <sujal7455@gmail.com>
2025-06-19 23:15:48 +0530

    constants assignment

--- 4_constants/constants.go
+++ 4_constants/constants.go
@@ -7,8 +7,19 @@ const age = 30 // possible outside main
 // const := "hello" // not possible outside main
 
 func main() {
-	const name string = "golang"
+	// const name string = "golang"
 
-	// name = "javascript" wrong // cannot assign to name
-	fmt.Println(name, age)
+	// // name = "javascript" wrong // cannot assign to name
+	// fmt.Println(name, age)
+
+	const (
+		port = 5000
+		host = "localhost"
+	)
+	// port = 8000 // cannot assign to constant  port
+
+	fmt.Println(port, host)
+
+
+
 }
