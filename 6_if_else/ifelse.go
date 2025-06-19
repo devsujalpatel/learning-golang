31c1b395a59052e064c315e53a3fd93727244cef
Sujal Patel <sujal7455@gmail.com>
2025-06-20 00:01:53 +0530

    else if

--- 6_if_else/ifelse.go
+++ 6_if_else/ifelse.go
@@ -1,13 +1,20 @@
 package main
 
-import "fmt"
-
 func main() {
-	age := 14
-	if age >= 18 {
-		fmt.Println("You are an adult")
-	} else {
-		fmt.Println("You are not an adult")
-	}
-	 
+	// age := 14
+	// if age >= 18 {
+	// 	fmt.Println("You are an adult")
+	// } else {
+	// 	fmt.Println("You are not an adult")
+	// }
+	// age := 18
+	
+	// if age >= 18 {
+	// 	fmt.Println("You are an adult")
+	// } else if age >= 12 {
+	// 	fmt.Println("You are a teenager")
+	// } else {
+	// 	fmt.Println("You are not an adult")
+	// }
+
 }
