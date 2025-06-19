b16486c64f0fe15ebd836dff3710dd34ac33a876
Sujal Patel <sujal7455@gmail.com>
2025-06-19 22:49:29 +0530

    shorthand syntax

---
+++ 3_variables/variables.go
@@ -0,0 +1,20 @@
+package main 
+
+import "fmt"
+
+func main() {
+	// var name string = "golang"
+	//infer
+	//var is variable
+	// var name = "golang"
+
+	var age int = 10
+	// shorthand  syntax
+	name := "golang"
+
+	var hello string
+	hello = "Hello Ji"
+
+	fmt.Println(hello, name, "you are", age, "years old")
+
+}
