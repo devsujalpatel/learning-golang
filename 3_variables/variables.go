70613c709b8c9607faa8a2f763e10a0e75ce90ee
Sujal Patel <sujal7455@gmail.com>
2025-06-19 22:52:18 +0530

    shortand with float

--- 3_variables/variables.go
+++ 3_variables/variables.go
@@ -8,13 +8,20 @@ func main() {
 	//var is variable
 	// var name = "golang"
 
-	var age int = 10
-	// shorthand  syntax
-	name := "golang"
+	// var age int = 10
+	// // shorthand  syntax
+	// name := "golang"
 
-	var hello string
-	hello = "Hello Ji"
+	// var hello string
+	// hello = "Hello Ji"
 
-	fmt.Println(hello, name, "you are", age, "years old")
+
+
+	// fmt.Println(hello, name, "you are", age, "years old")
+
+	// var price float32 = 50.5
+	price := 50.5
+
+	fmt.Println(price)
 
 }
