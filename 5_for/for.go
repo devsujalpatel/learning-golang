f888946f9af5bf81a0bb88333bc7778e8f95f4a8
Sujal Patel <sujal7455@gmail.com>
2025-06-19 23:23:38 +0530

    infinite, classic for loop, continue and break

--- 5_for/for.go
+++ 5_for/for.go
@@ -1,12 +1,36 @@
 package main
 
+import "fmt"
 
 // for loop -> only construct in go for looping
 func main () {
 	// while loop
-	i := 1
-	for i <= 5 {
-		println(i)
-		i++
+	// i := 1
+	// for i <= 5 {
+	// 	println(i)
+	// 	i++
+	// }
+	// Infinite while loop
+	// for {
+	// 	i++
+	// 	println("hello", i)
+	// }
+
+	// Classic for loop
+
+	for i := 0; i <= 3; i++ {
+		// break
+		if i == 2 {
+			break
+		}
+		// continue 
+		// if i == 2 {
+		// 	continue
+		// }
+			fmt.Println(i)
+		
 	}
+
+
+
 }
