1da0726cc2a053fc518c78f536383f0cdf2e0522
Sujal Patel <sujal7455@gmail.com>
2025-06-24 23:37:39 +0530

    changing value by reference

--- 15_pointers/pointers.go
+++ 15_pointers/pointers.go
@@ -1,13 +1,22 @@
 package main
 
-func changeNum(num int) {
-	num = 5
-	println("In changeNum", num)
+
+// by value
+// func changeNum(num int) {
+// 	num = 5
+// 	println("In changeNum", num)
+// }
+
+// by reference
+func changeNum(num *int) {
+	*num = 5
+	println("In changeNum", *num)
 }
 
 func main() {
 	num := 1
-	changeNum(num)
-
+	println(num)
+	changeNum(&num)
+	// println("Memory address", &num)
 	println("After changeNum in main", num)
 }
