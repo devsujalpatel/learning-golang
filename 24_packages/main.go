8db3d02ba5798d0a3586e9133c797a07be7ef9b9
Sujal Patel <sujal7455@gmail.com>
2025-06-27 02:43:27 +0530

    used color package from

--- 24_packages/main.go
+++ 24_packages/main.go
@@ -1,22 +1,27 @@
 package main
 
 import (
-	"fmt"
-	
+	// "fmt"
 
-	"github.com/devsujalpatel/podcast/auth"
+	// "github.com/devsujalpatel/podcast/auth"
 	"github.com/devsujalpatel/podcast/user"
+	"github.com/fatih/color"
 )
 
 func main() {
-	auth.LoginWithCredentials("sujal", "sujal123")
+	// auth.LoginWithCredentials("sujal", "sujal123")
 
-	session := auth.GetSession()
+	// session := auth.GetSession()
 
-	fmt.Println(session)
+	// fmt.Println(session)
 
-	user := user.User{Name: "Sujal", Age: 18}
-	fmt.Println(user.GetName())
-	fmt.Println(user.GetAge())
+	user := user.User{Name: "Sujal", Age: "18"}
+	// fmt.Println(user.GetName())
+	// fmt.Println(user.GetAge())
+
+	color.Red("Hello World!")
+	color.Blue(user.GetName())
+	age := user.GetAge()
+	color.HiGreen(age)
 
 }
--- 24_packages/user/user.go
+++ 24_packages/user/user.go
@@ -2,13 +2,13 @@ package user
 
 type User struct {
 	Name string
-	Age  int
+	Age  string
 }
 
 func (u User) GetName() string {
 	return u.Name
 }
 
-func (u User) GetAge() int {
+func (u User) GetAge() string {
 	return u.Age
 }
