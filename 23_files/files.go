5623a5af7ac5e38e06d44a84ff7de8696d2dadd7
Sujal Patel <sujal7455@gmail.com>
2025-06-27 00:53:37 +0530

    reading file in go

---
+++ 23_files/files.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	// f, err := os.Open("example.txt")
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// fileInfo, err := f.Stat()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println("file name:", fileInfo.Name())
+	// fmt.Println("file or dir:", fileInfo.IsDir())
+	// fmt.Println("file size:", fileInfo.Size())
+	// fmt.Println("file permissions:", fileInfo.Mode())
+	// fmt.Println("file modified at:", fileInfo.ModTime())
+
+	// Read file 
+
+	f, err := os.Open("example.txt")
+	if err != nil {
+		panic(err)
+	}
+	
+	defer f.Close()
+
+	fileInfo, err := f.Stat()
+	if err != nil {
+		panic(err)
+	}
+	size := fileInfo.Size()
+	
+	buf := make([]byte, size)
+
+	d, err := f.Read(buf)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("read", d, "bytes")
+	fmt.Println("data", string(buf))
+
+}
